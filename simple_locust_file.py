14d7d05e993901608edd85c94f37dd6ccf10c2ba
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-04-21 16:39:07 -0500

    refactors some things; adds a purchase workflow

--- simple_locust_file.py
+++ simple_locust_file.py
@@ -23,21 +23,33 @@ class GatlingDemoStore(HttpUser):
             login_data["_csrf"] = csrf
         self.client.post("/login", data=login_data)
 
-    def get_category(self):
+    def get_all_products(self):
         self.client.get("/category/all")
 
     def add_to_cart(self, item_number):
         self.client.get("/cart/add/{}".format(item_number), name="/cart/add/{item}")
+
+    def view_cart(self):
+        self.client.get("/cart/view")
+
+    def checkout(self):
+        self.client.get("/cart/checkout")
+
+    def clear_cart(self):
+        self.client.get("/cart/clear")
 
     @task
     def login_task(self):
         self.login_store()
 
     @task
-    def get_category_task(self):
-        self.get_category()
+    def get_all_products_task(self):
+        self.get_all_products()
 
     @task
-    def add_to_cart_task(self):
+    def purchase_workflow(self):  # flow enforced by website
+        self.get_all_products()
         item = random.randint(19, 25)
         self.add_to_cart(item)
+        self.view_cart()
+        self.checkout()
