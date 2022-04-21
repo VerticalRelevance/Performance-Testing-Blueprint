a79585c3156c65371101adca2c9d38be349cdccb
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-04-21 15:09:30 -0500

    refactors login

--- simple_locust_file.py
+++ simple_locust_file.py
@@ -10,11 +10,14 @@ def get_csrf_token(html):
 class GatlingDemoStore(HttpUser):
     host = "https://demostore.gatling.io"
 
-    @task
-    def login(self):
+    def login_store(self):
         login_data = {"username": "john", "password": "pass"}
         with self.client.get("/login", stream=True) as response:
             html = response.raw
             csrf = get_csrf_token(html)
             login_data["_csrf"] = csrf
         self.client.post("/login", data=login_data)
+
+    @task
+    def login_task(self):
+        self.login_store()
