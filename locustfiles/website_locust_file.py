f0ba6bb1a7ede849e3658cb24e84266ead5cf6d0
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-04-22 20:05:34 -0500

    WebsiteUser is now injected into contructor

--- locustfiles/website_locust_file.py
+++ locustfiles/website_locust_file.py
@@ -9,41 +9,41 @@ from website.website_user import WebsiteUser
 class WebsiteRunner(HttpUser):
     host = "https://demostore.gatling.io"
     wait_time = between(0.5, 2)
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = WebsiteUser(self.client)
 
     @task
     def login_task(self):
-        user = WebsiteUser(self.client)
-        user.login_store()
+        self.user.login_store()
 
     @task
     def get_all_products_task(self):
-        user = WebsiteUser(self.client)
-        user.get_products_in_category(ProductCategories.all)
+        self.user.get_products_in_category(ProductCategories.all)
 
     # should probably add some waits to these workflows
     @task
     def purchase_workflow(self):  # flow enforced by website
-        user = WebsiteUser(self.client)
-        user.get_products_in_category(ProductCategories.all)
+        self.user.get_products_in_category(ProductCategories.all)
         time.sleep(1)
         item = random.randint(19, 25)
-        user.add_to_cart(item)
+        self.user.add_to_cart(item)
         time.sleep(0.5)
-        user.view_cart()
+        self.user.view_cart()
         time.sleep(0.5)
-        user.checkout()
+        self.user.checkout()
         time.sleep(0.5)
 
     @task
     def browse_workflow(self):
-        user = WebsiteUser(self.client)
-        user.get_products_in_category(ProductCategories.for_him)
+        self.user.get_products_in_category(ProductCategories.for_him)
         time.sleep(2)
         available_products = ["casual-black-blue", "black-and-red-glasses"]
         item_key = random.choice(available_products)
-        user.get_product(item_key)
+        self.user.get_product(item_key)
         time.sleep(0.5)
         available_products = ["casual-black-blue", "black-and-red-glasses"]
         item_key = random.choice(available_products)
-        user.get_product(item_key)
+        self.user.get_product(item_key)
         time.sleep(0.5)
