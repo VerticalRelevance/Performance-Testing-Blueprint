72a45749532e3c1b53dda04810bfeb91d3e43028
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-04-25 16:55:10 -0500

    refactors

--- components/categories.py
+++ components/categories.py
@@ -7,10 +7,11 @@ class Categories:
         self.client = client
         self.headers = {"accept": "application/json"}
         self.endpoint = "/api/category"
+        self.category_ids = [5, 6, 7]
 
     def get_all(self):
         self.client.get(self.endpoint, headers=self.headers)
 
     def get_by_id(self):
-        category_id = random.choice([5, 6, 7])
+        category_id = random.choice(self.category_ids)
         self.client.get(self.endpoint + "/{}".format(category_id), headers=self.headers)
--- locustfiles/categories_locust_file.py
+++ locustfiles/categories_locust_file.py
@@ -4,7 +4,7 @@ from components.categories import Categories
 
 class ApiTester(HttpUser):
     host = "http://demostore.gatling.io"
-    wait_time = constant_throughput(1)  # number of requests per second per user
+    wait_time = constant_throughput(1)  # number of @tasks per second per user
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
