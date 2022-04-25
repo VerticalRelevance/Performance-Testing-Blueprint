9d25158cb4640ec3e2bbdbed8a9cbf84b2f5ab79
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-04-25 18:52:57 -0500

    refactors

--- components/product.py
+++ components/product.py
@@ -8,12 +8,13 @@ class Product:
         self._headers = {"accept": "application/json"}
         self._endpoint = "/product/{}"
         self._products_for_him = ["casual-black-blue", "black-and-red-glasses", "bright-yellow-glasses",
-                                 "casual-brown-glasses", "deepest-blue", "light-blue-glasses",
-                                 "sky-blue-case", "white-casual-case"]
-        self._products_for_her = ["perfect-pink", "curved-black", "black-grey-curved", "black-light-blue", "curved-pink",
-                                 "velvet-red", "deep-blue-ocean"]
+                                  "casual-brown-glasses", "deepest-blue", "light-blue-glasses",
+                                  "sky-blue-case", "white-casual-case"]
+        self._products_for_her = ["perfect-pink", "curved-black", "black-grey-curved", "black-light-blue",
+                                  "curved-pink",
+                                  "velvet-red", "deep-blue-ocean"]
         self._products_for_unisex = ["curved-brown", "leopard-skin", "gold-design", "pink-panther", "curve-ocean-sky",
-                                    "plain-white", "white-leopard-pattern"]
+                                     "plain-white", "white-leopard-pattern"]
 
     def _get_product(self, path):
         self._client.get(path, headers=self._headers)
