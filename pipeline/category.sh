2a62a6cbd1f3db64400aabbcfb9fc32bfa145563
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-04-26 07:41:34 -0500

    refactors pipeline script

--- pipeline/category.sh
+++ pipeline/category.sh
@@ -1,10 +1,15 @@
 data_tag=$1
 
+number_of_users=10
+spawn_rate=2
+run_time=1m
+
 # capture configurations
 # TODO: logic here to capture some things
 
 # run the tests
-locust -f locustfiles/category_locust_file.py -u 10 -r 2 -t 1m --headless --csv "$data_tag"
+locust -f locustfiles/category_locust_file.py -u "$number_of_users" -r "$spawn_rate" -t "$run_time" \
+ --headless --csv "$data_tag"
 
 # present the results
 #  TODO: logic here to analyze results for pass/fail exit code
