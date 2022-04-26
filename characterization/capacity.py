af735e7aeeefdef5a3a734dda6bfbe044f260063
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-04-26 10:27:05 -0500

    implements dwell in time units

--- characterization/capacity.py
+++ characterization/capacity.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import math
 
 from locust import TaskSet, task, HttpUser, constant_throughput, LoadTestShape
@@ -13,11 +14,21 @@ class UserTasks(TaskSet):
         Category(self.client).get_category()  # TODO: inject other components and user journeys
 
 
+class Configuration:
+    user_throughput = 1  # number of tasks every second per user
+    time_limit = 300  # in seconds
+    initial_number_of_users = 1
+    spawn_rate = 9001  # no-op? Used only for printing to console?
+    user_step = 1
+    dwell = 10  # seconds to wait for an increase in number of users
+    max_users = 20
+
+
 class WebsiteUser(HttpUser):
     """
     Define the load characteristics and specify tasks.
     """
-    wait_time = constant_throughput(1)
+    wait_time = constant_throughput(Configuration.user_throughput)
     host = "https://demostore.gatling.io"
     tasks = [UserTasks]
 
@@ -26,20 +37,23 @@ class Capacity(LoadTestShape):
     """
     Define the load shape.
     """
-    _time_limit = 120  # in seconds
-    _number_of_users = 1
+    _number_of_users = Configuration.initial_number_of_users
+    _tick_counter = 0
 
-    def tick(self):  # TODO: need some insight into test results here to help dynamically calculate shape
+    # TODO: need some insight into test results here to help dynamically calculate shape
+    def tick(self):  # 1 tick = 1s
         """
         Method that defines the shape at an instance of time, i.e. a "tick".
         :return: Tuple of number of users and spawn rate.
         """
-        spawn_rate = 0.05  # dwell time baked in to < 1 spawn rate
-
         run_time = self.get_run_time()
 
-        if run_time > self._time_limit:
+        if run_time > Configuration.time_limit:  # stop at end of time limit
             return None
-        self._number_of_users += spawn_rate
-        step_number_of_users = math.floor(self._number_of_users)
-        return step_number_of_users, spawn_rate
+        if self._number_of_users == Configuration.max_users:
+            return None
+        if self._tick_counter == Configuration.dwell:  # add users if dwell reached
+            self._number_of_users += Configuration.user_step
+            self._tick_counter = 0
+        self._tick_counter += 1
+        return self._number_of_users, Configuration.spawn_rate
