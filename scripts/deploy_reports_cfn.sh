3e8d9b3db977f991a89b2e347140db3f6920c00d
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-05-04 14:06:50 -0500

    adds script and template for creating performance reports s3 bucket

---
+++ scripts/deploy_reports_cfn.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+template=../templates/reports.yml
+stack_name=PerformanceTestingReports
+reports_s3_bucket_name=performance-test-reports-nchandlersmith
+
+echo Preparing to deploy template reports.yml
+
+echo Linting...
+cfn-lint "$template"
+
+echo Deploying...
+aws cloudformation deploy \
+    --template-file "$template" \
+    --stack-name "$stack_name" \
+    --parameter-overrides \
+        PerformanceReportsS3Bucket="$reports_s3_bucket_name"
