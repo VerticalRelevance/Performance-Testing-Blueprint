d8a530629855e33cc7588ea9a2a90226a9a119ab
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-05-04 14:44:38 -0500

    adds script and template for creating image repo

---
+++ scripts/deploy_images_cfn.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+template=../templates/images.yml
+stack_name=PerformanceTestingImages
+image_repo_name=locust-performance-test
+user_arn="$AWS_PERFORMANCE_TEST_USER_ARN"
+
+
+echo Preparing to deploy template reports.yml
+
+echo Linting...
+cfn-lint "$template"
+
+echo Deploying...
+aws cloudformation deploy \
+    --template-file "$template" \
+    --stack-name "$stack_name" \
+    --parameter-overrides \
+        LocustImageRepositoryName="$image_repo_name" \
+        PerformanceTestUserArn="$user_arn"
