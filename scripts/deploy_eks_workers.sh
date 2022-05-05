afa9e267b0baa77b198649505c3517262b5b78b9
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-05-05 14:31:26 -0500

    changes the order of some things

--- scripts/deploy_eks_workers.sh
+++ scripts/deploy_eks_workers.sh
@@ -4,9 +4,7 @@ template=../templates/eks_workers.yml
 stack_name=PerformanceTestingEKSWorkers
 
 # things from the control plane stack
-control_plane_stack_name=PerformanceTestingEKSControlPlane
 cluster_name=PerformanceTestingControlPlane
-cluster_control_plane_security_group=sg-05134c68fb6c4a287
 key_name=EKSWorkersKey
 desired_capacity=1
 max_capacity=3
@@ -14,6 +12,8 @@ min_capacity=1
 worker_node_group_name=PerformanceTestWorkers
 node_instance_type=t3.micro
 node_volume_size=8
+# These three come from control plan
+cluster_control_plane_security_group=sg-05134c68fb6c4a287
 subnets=subnet-06131d9b35c5e3eec,subnet-08710858dcf04c97a,subnet-0fc6f3f75ff5d142e,subnet-0faa75694a234d0e0
 vpc_id=vpc-0065967fbb6fba9ba
 
@@ -29,7 +29,6 @@ aws cloudformation deploy \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
       ClusterName="$cluster_name" \
-      ClusterControlPlaneSecurityGroup="$cluster_control_plane_security_group" \
       KeyName="$key_name" \
       NodeAutoScalingGroupDesiredCapacity="$desired_capacity" \
       NodeAutoScalingGroupMaxSize="$max_capacity" \
@@ -37,5 +36,6 @@ aws cloudformation deploy \
       NodeGroupName="$worker_node_group_name" \
       NodeInstanceType="$node_instance_type" \
       NodeVolumeSize="$node_volume_size" \
+      ClusterControlPlaneSecurityGroup="$cluster_control_plane_security_group" \
       Subnets="$subnets" \
       VpcId="$vpc_id" \
