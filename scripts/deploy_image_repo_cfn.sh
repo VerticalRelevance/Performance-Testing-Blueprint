b8b3f7438f75c536048eaf47d07882d0d362b5b9
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-05-09 09:58:34 -0500

    generalizes ecr creation

--- scripts/deploy_image_repo_cfn.sh
+++ scripts/deploy_image_repo_cfn.sh
@@ -3,7 +3,6 @@
 template=../templates/ecr.yml
 stack_name=PerformanceTestingImageRepo
 repo_name=locust-image-performance-test
-user_arn="$AWS_PERFORMANCE_TEST_USER_ARN"
 
 
 echo Preparing to deploy images template
@@ -16,5 +15,4 @@ aws cloudformation deploy \
     --template-file "$template" \
     --stack-name "$stack_name" \
     --parameter-overrides \
-        RepositoryName="$repo_name" \
-        PerformanceTestUserArn="$user_arn"
+        RepositoryName="$repo_name"
