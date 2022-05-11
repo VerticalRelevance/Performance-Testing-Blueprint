558c0681f6dc0b402b1b430e01279f318130cd8a
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-05-11 16:05:11 -0500

    blueprint successfully runs on eks

--- scripts/install_chart.sh
+++ scripts/install_chart.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
 
-echo Run a kubectl to check connection
-kubectl get service
-
-# enable experimental OCI support
-export HELM_EXPERIMENTAL_OCI=1
-
-echo Authenticating with AWS ECR...
-aws ecr get-login-password \
-  --region "$AWS_REGION" | helm registry login \
-  --username AWS \
-  --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr."$AWS_REGION".amazonaws.com
-
-echo Pull chart to local...
-echo Pull Helm chart to local cache
-helm pull oci://"$AWS_ACCOUNT_ID".dkr.ecr."$AWS_REGION".amazonaws.com/locust-chart-performance-test
+release_name=$1
 
 echo Installing...
-helm install locust-performance-tester ./locust-chart-performance-test-0.26.1.tgz
+helm install "$release_name" deliveryhero/locust \
+  --set loadtest.name=blueprint \
+  --set loadtest.locust_locustfile_configmap=blueprint-locustfile \
+  --set loadtest.locust_lib_configmap=example-lib \
+  -f ../chart/values.yaml
