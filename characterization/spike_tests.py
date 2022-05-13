a7091a30b8ce7eddbe13a9b7d1ecf54f2175a7b1
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-05-13 13:16:18 -0500

    spike test honors custom args

--- characterization/spike_tests.py
+++ characterization/spike_tests.py
@@ -3,21 +3,20 @@ from locust import TaskSet, task, HttpUser, constant_throughput, LoadTestShape, 
 from website.user_journey import UserJourney
 
 
-# TODO: fix these parameters. They don't work!
 @events.init_command_line_parser.add_listener
 def _(parser):
     parser.add_argument(
         "--steady-state",
         type=int,
         env_var="LOCUST_STEADY_STATE",
-        default=1,
+        default=10,
         help="Pick a 'high' number that will not cause failures.")
 
     parser.add_argument(
         "--user-increment",
         type=int,
         env_var="LOCUST_USER_INCREMENT",
-        default=1,
+        default=10,
         help="Number of users to increment up or down to inject or remove a spike. "
              "Pick a number of to add that will cause failures.")
 
@@ -25,7 +24,7 @@ def _(parser):
         "--dwell",
         type=int,
         env_var="LOCUST_DWELL",
-        default=1,
+        default=10,
         help="Amount of time in seconds to wait before adding or removing users.")
 
 
@@ -43,10 +42,10 @@ class UserTasks(TaskSet):
         UserJourney(self.client).abandon_cart()
 
 
-class DataCaptor:
-    steady_state = 10
-    user_increment = 10
-    dwell = 10
+class CustomArgs:
+    steady_state = 0
+    user_increment = 1
+    dwell = 5
 
 
 class SpikeUser(HttpUser):
@@ -56,27 +55,26 @@ class SpikeUser(HttpUser):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        DataCaptor.steady_state = self.environment.parsed_options.steady_state
-        DataCaptor.user_increment = self.environment.parsed_options.user_increment
-        DataCaptor.dwell = self.environment.parsed_options.dwell
+        CustomArgs.steady_state = self.environment.parsed_options.steady_state
+        CustomArgs.user_increment = self.environment.parsed_options.user_increment
+        CustomArgs.dwell = self.environment.parsed_options.dwell
 
 
 class SpikeTestShaper(LoadTestShape):
     def __init__(self):
         super().__init__()
-        self._steady_state = DataCaptor.steady_state
-        self._user_increment = DataCaptor.user_increment
-        self._dwell = DataCaptor.dwell
         self._tick_counter = 0
-        self._number_of_users = DataCaptor.steady_state
+        self._number_of_users = 0
 
     def tick(self):
         self._tick_counter += 1
-        if self._tick_counter >= self._dwell:
+        if self._number_of_users < CustomArgs.steady_state:
+            self._number_of_users = CustomArgs.steady_state
+        elif self._tick_counter >= CustomArgs.dwell:
             self._tick_counter = 0
-            if self._number_of_users == self._steady_state:
-                self._number_of_users += self._user_increment
+            if self._number_of_users == CustomArgs.steady_state:
+                self._number_of_users += CustomArgs.user_increment
             else:
-                self._number_of_users -= self._user_increment
-        return self._number_of_users, self._user_increment
+                self._number_of_users -= CustomArgs.user_increment
+        return self._number_of_users, CustomArgs.user_increment
 
