e1f1701c1b07b5e92896de26e0a7b191b85300ed
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-05-13 17:17:47 -0500

    refactors

--- controls/spike_shape_controller.py
+++ controls/spike_shape_controller.py
@@ -17,9 +17,9 @@ class SpikeShapeController:
         self._current_users = 0
         self._state = "Not Initialized"
         self.state_dictionary = {
-            "Not Initialized": self.not_initialized,
-            "Ramping To Steady State": self.ramping_to_steady_state,
-            "Idling At Steady State": self.idling_at_steady_state,
+            "Not Initialized": self._not_initialized,
+            "Ramping To Steady State": self._ramping_to_steady_state,
+            "Idling At Steady State": self._idling_at_steady_state,
             "Ramping To Spike State": self._ramping_to_spike_state,
             "Idling At Spike State": self._idling_at_spike_state
         }
@@ -28,17 +28,17 @@ class SpikeShapeController:
         self.state_dictionary[self._state](args)
         return self._current_users, args.spawn_rate
 
-    def not_initialized(self, args):
+    def _not_initialized(self, args):
         self._state = "Ramping To Steady State"
 
-    def ramping_to_steady_state(self, args: CustomArgs):
+    def _ramping_to_steady_state(self, args: CustomArgs):
         self._current_users += args.spawn_rate
         if self._current_users > args.steady_state_users:
             self._current_users = args.steady_state_users
         if self._current_users == args.steady_state_users:
             self._state = "Idling At Steady State"
 
-    def idling_at_steady_state(self, args:CustomArgs):
+    def _idling_at_steady_state(self, args:CustomArgs):
         self._tick_counter += 1
         if args.steady_state_dwell - 1 == self._tick_counter:
             self._state = "Ramping To Spike State"
