094491e37a6a23a1640afe0989c7b3f86e1b92ba
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-05-17 09:44:35 -0500

    adds some comments around how to use Locust

--- locustfiles/simple_locust_file.py
+++ locustfiles/simple_locust_file.py
@@ -2,8 +2,15 @@ from locust import HttpUser, task
 
 
 class SimplePerformanceTest(HttpUser):
+    """
+    Extend HttpUser class. This class has the httpclient on it that is used to send http requests.
+    """
+
+    # Specify the host to attack here
     host = "http://demostore.gatling.io"
 
+    # The @task annotation is used to tell Locust what the test methods are. Note the use of self.client inside the
+    # test method to send http requests.
     @task
     def get_all_categories(self):
         self.client.get("/api/category", headers={"accept": "application/json"})
