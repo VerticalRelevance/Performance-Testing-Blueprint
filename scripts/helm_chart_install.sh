90556d4691066819a403a6276013c613a03781ac
Nathan Chandler-Smith <nchandlersmith@verticalrelevance.com>
2022-05-22 16:50:18 -0500

    adds principal to EKSIAMRole

--- scripts/helm_chart_install.sh
+++ scripts/helm_chart_install.sh
@@ -3,11 +3,11 @@
 release_name=$1
 
 echo Creating locustfile configmap...
-kubectl create configmap blueprint-locustfile --from-file ../locustfiles/blueprint.py
+kubectl create configmap blueprint-locustfile --from-file locustfiles/blueprint.py
 
 echo Installing...
 helm install "$release_name" deliveryhero/locust \
   --set loadtest.name=blueprint \
   --set loadtest.locust_locustfile_configmap=blueprint-locustfile \
   --set loadtest.locust_lib_configmap=example-lib \
-  -f ../chart/values.yaml
+  -f chart/values.yaml
